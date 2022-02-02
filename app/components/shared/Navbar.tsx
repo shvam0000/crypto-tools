0e42aa6979636da271ad7d2e7feeae803d32824a
shvam0000 <shvam0000@gmail.com>
2022-02-02 13:35:53 +0530

    :zap: update navbar

--- app/components/shared/Navbar.tsx
+++ app/components/shared/Navbar.tsx
@@ -4,18 +4,23 @@ import { Link } from 'remix';
 const Navbar = () => {
   return (
     <div className='flex  justify-between items-center px-28 py-4 text-white'>
-      <div className='flex items-center justify-center text-xl font-bold'>
-        <span className='text-3xl text-[#38BDF8] mx-2'>
-          <Logo />
-        </span>
-        <span>Cryptography Tools</span>
-      </div>
-      <div className='flex justify-center items-center'>
+      <Link to='/'>
+        <div className='flex items-center justify-center text-xl font-bold'>
+          <span className='text-3xl text-[#38BDF8] mx-2'>
+            <Logo />
+          </span>
+          <span>Cryptography Tools</span>
+        </div>
+      </Link>
+      <div className='flex justify-center items-center font-semibold '>
         <span className='mx-2'>
           <Link to='/tools'>Tools</Link>
         </span>
         <span className='mx-2'>
           <Link to='/stegnography'>Stegnography</Link>
+        </span>
+        <span className='mx-2'>
+          <Link to='/tools'>About Us</Link>
         </span>
         <span className='mx-2 text-3xl text-gray-600'>
           <a href='https://github.com/shvam0000/crypto-tools'>
