e1c1d54678603ea85d2c166e45558fa9b8e1a1d3
shvam0000 <shvam0000@gmail.com>
2022-02-05 13:18:43 +0530

    :heavy_plus_sign: add hero image

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,10 @@
 import { About } from '~/components/HomePage';
-import { Player } from '@lottiefiles/react-lottie-player';
+import Hack from '~/utils/icons/Hack';
 
 export default function Index() {
   return (
-    <div>
-      <div className='h-96'>
-        <Player src='https://assets8.lottiefiles.com/packages/lf20_mwopkrz2.json' />
-      </div>
+    <div className='flex transform animate-pulse flex-col items-center justify-center'>
+      <Hack />
       <About />
     </div>
   );
--- app/utils/icons/index.tsx
+++ app/utils/icons/index.tsx
@@ -1,3 +1,4 @@
 export { default as Logo } from './Logo';
 export { default as GitHub } from './Github';
 export { default as UnderConstruction } from './UnderConstruction';
+export { default as Hero } from './Hack';
